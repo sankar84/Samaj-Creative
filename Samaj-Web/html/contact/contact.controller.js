6e14de54c19860a2f317d62927b99cffc3408a47
sankar-epsilon <sankar.anbazhagan@epsilon.com>
2015-11-15 10:06:38 -0500

    changes for contact us page

--- Samaj-Web/html/contact/contact.controller.js
+++ Samaj-Web/html/contact/contact.controller.js
@@ -4,17 +4,43 @@
 	angular
 		.module('app')
 		.controller('contact.controller', ContactController);
+	
+	angular
+	.module('app')
+	.directive('userinfo', function() {
+	    var directive = {};
+
+	    directive.restrict = 'E';
+
+	    directive.template = "User : {{user}}";
+
+	    directive.scope = {
+	        user : "@user"
+	    }
+
+	    return directive;
+	})	
 
 	ContactController.$inject = ['$rootScope', '$scope', '$compile'];
 
 	function ContactController($rootScope, $scope, $compile) {
 		var vm = this;
+		vm.form = {};
+		vm.formData = {};
+		vm.login = login;
 		
 		activate();
 
 		// activate function to be ran when this page is viewed
 		function activate() {
 		}
+		
+		function login(isValid) {
+			if(isValid) {
+				alert("Data entered : "+vm.formData.name+","+vm.formData.email+","+vm.formData.comment);
+				vm.formData.name = "New Name";
+			}
+		};		
 		
 	};
 
